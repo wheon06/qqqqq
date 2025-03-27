8a21feaa055d04b0fd6757378418104e2d523519
이희연 HeeYeon Lee <up2kid@gmail.com>
2025-03-27 10:56:34 +0900

    asdsada

--- src/test/java/com/wheon/dockercompleate/DockerCompleateApplicationTests.java
+++ src/test/java/com/wheon/dockercompleate/DockerCompleateApplicationTests.java
@@ -3,7 +3,7 @@ package com.wheon.dockercompleate;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = DockerCompleateApplication.class)
+@SpringBootTest(classes = DockerCompleateApplicationTests.class)
 class DockerCompleateApplicationTests {
 
     @Test
